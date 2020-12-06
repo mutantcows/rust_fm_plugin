bbef52e3fbcd55d60a60b2d859746ca5e7fd3e92
mutantcows <mutantcows@gmail.com>
2020-12-05 19:52:00 -0500

    simplify convert to base

--- src/functions/convert_to_base.rs
+++ src/functions/convert_to_base.rs
@@ -1,5 +1,4 @@
 use crate::ffi::*;
-use crate::helpers::*;
 use std::os::raw::c_short;
 
 #[no_mangle]
@@ -13,7 +12,7 @@ pub(crate) unsafe extern "C" fn rust_convert_to_base(
 
     let number = data_vect.at(0);
     let out_locale = number.get_locale();
-    let mut number: fmx_int32 = i32::from(number);
+    let mut number = i32::from(number);
 
     let base = data_vect.at_as_number(1);
     let base: fmx_int32 = i32::from(base);
@@ -23,11 +22,9 @@ pub(crate) unsafe extern "C" fn rust_convert_to_base(
         _ => return 960,
     }
 
-    let mut out_text = Text::new();
-    let mut insert_buffer = Text::new();
-
+    let mut text = String::new();
     if number == 0 {
-        out_text.assign("0");
+        text.push('0');
     } else {
         let neg = number < 0;
         if neg {
@@ -36,15 +33,16 @@ pub(crate) unsafe extern "C" fn rust_convert_to_base(
         while number > 0 {
             let digit = (number % base) as u8;
             let ch = if digit < 10 { b'0' } else { b'A' - 10 } + digit;
-            prepend_character(&mut out_text, &mut insert_buffer, ch as char);
+            text.insert(0, ch as char);
             number /= base;
         }
         if neg {
-            prepend_character(&mut out_text, &mut insert_buffer, '-');
+            text.insert(0, '-');
         }
     }
 
     let mut results = Data::from_ptr(results_ptr);
+    let out_text = Text::from(&text);
     results.set_as_text(out_text, out_locale);
     0
 }
