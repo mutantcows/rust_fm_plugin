7a536b9a8673862d1890759d56169af9c8181f56
mutantcows <mutantcows@gmail.com>
2020-12-27 23:05:22 -0500

    begin integration testing

---
+++ tests/integration_test.rs
@@ -0,0 +1,27 @@
+#[cfg(test)]
+mod test {
+    use std::process::Command;
+    use std::sync::Once;
+
+    static INIT: Once = Once::new();
+
+    pub fn initialize() {
+        INIT.call_once(|| {
+            fm_plugin::post_build::bundle_plugin().unwrap();
+        });
+    }
+    #[test]
+    fn test_pluggo() {
+        initialize();
+        let output = Command::new("cmd")
+            .arg("/c")
+            .arg("start")
+            .arg("fmp://~/plugin_test?script=test")
+            .output()
+            .expect("failed open fm");
+        let status = output.status;
+        assert_eq!(status.to_string(), "exit code: 0");
+        let result = std::str::from_utf8(&output.stdout).unwrap();
+        assert_eq!(result, "");
+    }
+}
