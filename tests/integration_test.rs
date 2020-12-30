67e3575a267fe5283055079388301acac7fb4b0b
mutantcows <mutantcows@gmail.com>
2020-12-30 14:45:58 -0500

    add version and socket functions to test plugin

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,5 +1,7 @@
 #[cfg(test)]
 mod test {
+    use std::io::prelude::*;
+    use std::net::TcpListener;
     use std::process::Command;
     use std::sync::Once;
 
@@ -22,13 +24,14 @@ mod test {
     #[test]
     fn test_pluggo() {
         initialize();
-        Command::new("cmd")
+        let output = Command::new("cmd")
             .arg("/c")
             .arg("start")
             .arg("test_plugin.fmp12")
             .current_dir("./tests")
             .output()
             .expect("couldn't open test db");
+        assert_eq!(output.status.to_string(), "exit code: 0");
 
         let output = Command::new("cmd")
             .arg("/c")
@@ -36,9 +39,24 @@ mod test {
             .arg("fmp://$/test_plugin?script=test")
             .output()
             .expect("failed open fm");
-        let status = output.status;
-        assert_eq!(status.to_string(), "exit code: 0");
+        assert_eq!(output.status.to_string(), "exit code: 0");
         let result = std::str::from_utf8(&output.stdout).unwrap();
         assert_eq!(result, "");
+
+        let result = filemaker_listener().unwrap();
+        assert_eq!(result, "1");
+    }
+
+    fn filemaker_listener() -> Result<String, Box<dyn std::error::Error>> {
+        let listener = TcpListener::bind("127.0.0.1:12346")?;
+
+        let mut buffer: Vec<u8> = Vec::new();
+        for result in listener.incoming() {
+            let mut stream = result?;
+            stream.read_to_end(&mut buffer).unwrap();
+            break;
+        }
+
+        Ok(std::str::from_utf8(&buffer)?.to_owned())
     }
 }
--- tests/test_plugin/src/lib.rs
+++ tests/test_plugin/src/lib.rs
@@ -1,5 +1,7 @@
 use fm_plugin::prelude::*;
 use fm_plugin::{Data, DataVect, ExprEnv, ScriptControl, Text};
+use std::io::prelude::*;
+use std::net::TcpStream;
 
 struct TestPlugin;
 
@@ -56,6 +58,31 @@ impl Plugin for TestPlugin {
             min_version: ExternVersion::V160,
             function_ptr: Some(TestScriptStep::extern_func),
         },
+        Registration::Function {
+            id: 300,
+            name: "TEST_Version",
+            definition: "TEST_Version",
+            description: "Test plugin version",
+            min_args: 0,
+            max_args: 0,
+            display_in_dialogs: true,
+            compatibility_flags: Compatibility::Future as u32,
+            min_version: ExternVersion::V160,
+            function_ptr: Some(TestVersion::extern_func),
+        },
+        Registration::Function {
+            id: 400,
+            name: "TEST_Socket",
+            definition: "TEST_Socket ( address ; message )",
+            description: "Send message over socket.",
+            min_args: 2,
+            max_args: 2,
+            display_in_dialogs: true,
+            compatibility_flags: Compatibility::Future as u32,
+            min_version: ExternVersion::V160,
+            function_ptr: Some(TestSocket::extern_func),
+        },
+
 ]
     }
 }
@@ -72,6 +99,28 @@ struct TestScriptStep;
 
 impl FileMakerFunction for TestScriptStep {
     fn function(_id: i16, _env: &ExprEnv, _args: &DataVect, _result: &mut Data) -> FMError {
+        FMError::NoError
+    }
+}
+
+struct TestVersion;
+
+impl FileMakerFunction for TestVersion {
+    fn function(_id: i16, _env: &ExprEnv, _args: &DataVect, result: &mut Data) -> FMError {
+        result.set_as_number(1);
+        FMError::NoError
+    }
+}
+
+struct TestSocket;
+
+impl FileMakerFunction for TestSocket {
+    fn function(_id: i16, _env: &ExprEnv, args: &DataVect, _result: &mut Data) -> FMError {
+        let address = args.at_as_string(0);
+        let msg = args.at_as_string(1);
+        let mut stream = TcpStream::connect(address).unwrap();
+
+        stream.write(msg.as_bytes()).unwrap();
         FMError::NoError
     }
 }
